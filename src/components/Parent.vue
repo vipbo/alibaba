7f1b047b175835340744eb8e3f468fd645a4f754
vipbo <253255657@qq.com>
2019-08-14 01:46:57 +0800

    'v-model'

--- src/components/Parent.vue
+++ src/components/Parent.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <Son1 :message="message" @changeMore="change"/>
+    <!-- <Son1 :message="message" @changeMore="change"/> 写法1-->
+    <!-- <Son1 :message="message" @changeMore="value=>this.message=value"/> 写法2-->
+    <!-- <Son1 :message="message" @update:message="value=>this.message=value"/> 写法3-->
+    <!-- <Son1 :message.sync="message"/> 写法4 是写法3的简化形式 -->
+    <!-- <Son1 :message="message" @input="value=>this.message = value"/> -->
+    <Son1 v-model="message"/>
+    <!-- v-model 是 value 和 input事件的简写形式，且只能是value和input，而sync就自由灵活的多 -->
   </div>
 </template>
 
@@ -22,9 +28,9 @@ export default {
     }
   },
   methods:{
-    change(value){
-      this.message = value;
-    }
+    // change(value){ 写法1
+    //   this.message = value;
+    // }
   }
 }
 </script>
--- src/components/Son1.vue
+++ src/components/Son1.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="son1">
         <p>Son1</p>
-        <p>Son1-{{message}}</p> 
+        <p>message-{{message}}</p> 
+        <p>value-{{value}}</p> 
         <p @click="change" class="change-more">点击变多</p>
     </div>
 </template>
 <script>
 export default {
     props:{
+        value:{
+            type:Number,
+            default:8
+        },
         message:{
             type:Number,
             default:10
@@ -15,7 +20,9 @@ export default {
     },
     methods:{
         change(){
-            this.$emit('changeMore',200)
+            // this.$emit('changeMore',200) // 写法1 写法2
+            // this.$emit('update:message',300) // 写法3 写法4
+            this.$emit('input',300) // 写法3 写法4
         }
     }
 }
